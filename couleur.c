209759eb3881a49943d6a3c3df48524bc1826b6c
Vartan <vartan741@hotmail.fr>
2017-03-10 11:46:06 +0100

    '

---
+++ couleur.c
@@ -0,0 +1,30 @@
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include "couleur.h"
+	
+#define couleur(param) printf("\033[%sm",param)
+
+
+int main(){
+
+
+
+couleur("30");
+printf("test\n");
+couleur("0");
+
+
+
+
+return 0;
+
+}
+
+
