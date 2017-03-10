cfa810e83cbaad09340e065002adde43118e25a6
Kevin <kevin170895fr@live.fr>
2017-03-10 12:19:11 +0100

    maj doxygen

--- coul-fct1_2.h
+++ coul-fct1_2.h
@@ -9,8 +9,38 @@
 #include <math.h>
 #include <string.h>
 #include <time.h>
+
+
+/**
+ /file coul-fct1_2.h
+*/
+
 typedef struct coord coord;
 typedef int** matrice;
+
+/**
+ /function getche
+ /brief Fonction permettant aux joueurs d'interagir avec le jeu 
+*/
 int getche(void);
+
+/**
+ /function colorplace
+ /param M(matrice de caractère)
+ /param c(coordonnée)
+ /param color
+ /param m
+ /return matrix
+ /brief Prend les coordonnées c(x;y) d'une matrice pour changer par la couleur 'color' 
+*/
 matrix coloreplace(matrix M,coord c,char color,int m);
+
+/**
+ /function composante_conn
+ /param M(matrice de caractère)
+ /param color
+ /param m
+ /return matrice(d'entier)
+ /brief Renvoie une matrice d'entier donnant les cases à colorier (pas du tout optimal)
+*/
 matrice composante_conn(matrix M,char color,int m);
