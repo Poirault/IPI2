d650ea45bb77d37f841ec534eb66a5699457da66
Kevin <kevin170895fr@live.fr>
2017-03-10 12:05:17 +0100

    ok

---
+++ main2.c
@@ -0,0 +1,123 @@
+#include "coul-fct1_2.h"
+#include "grille.h"
+#include "victoire.h"
+#include "fonctions_utiles.h"
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+int main()
+{
+	int j,k,m,f,g, nbCoup, tour = 0;
+	f=1;
+
+	char o,c;
+
+	matrix M;
+	
+	matrice T;
+
+	printf("Donnez la taille du jeu\n");
+	scanf("%d", &m);
+
+	M=grille(m);
+
+	nbCoup = nmbre_coup(m);
+
+	system("clear");
+
+	for (j = 0; j < m; ++j)
+	{
+		for (k = 0; k < m; ++k)
+		{
+			
+			printf("%10c", M[j][k]); //affiche les couleurs avec 10 espaces entre les couleurs d'une même ligne
+		}
+	printf("\n");
+	} printf("Number tour : %d/%d \n", tour, nbCoup);
+
+	while((f!=0) && (f!=2)) { //Tant qu'il n'y a pas de victoire ou que le nombre de tour est inférieure ou égale aux nombres de coups
+		g=1;
+		printf("Choisissez une couleur\n");
+		while(g!=0){
+			o=getche();
+			if(o=='b'){
+				c='B';
+				g=0;
+			}
+			if(o=='g'){
+				c='G';
+				g=0;
+			}
+			if(o=='j'){
+				c='J';
+				g=0;
+			}
+			if(o=='m'){
+				c='M';
+				g=0;
+			}
+			if(o=='r'){
+				c='R';
+				g=0;
+			}
+			if(o=='v'){
+				c='V';
+				g=0;
+			}
+		}
+		
+		T=composante_conn(M,c,m); //Matrice d'entier pour savoir quoi "colorier"
+
+		for (j = 0; j < m; ++j)
+		{
+			for (k = 0; k < m; ++k)
+			{
+				if(T[j][k]==1)
+				{
+					M[j][k]=c; //on a pas besoin de la fct coloreplace
+				}
+			}
+		}
+
+		system("clear");
+
+		for (j = 0; j < m; ++j) //affiche le jeu avec les nouvelles couleurs
+		{
+			for (k = 0; k < m; ++k)
+			{
+				
+				printf("%10c", M[j][k]);
+			}
+		printf("\n");
+		}
+
+		tour++;
+		if (tour<=nbCoup) {
+			printf("Number tour : %d/%d \n", tour, nbCoup);
+		}
+
+		f=victoire(M,m,tour,nbCoup);
+	}
+
+	for (j = 0; j < m; ++j) //Libération d'espace mémoire
+		{
+			
+			free(M[j]);
+			free(T[j]);
+		
+		}
+		free(M);
+		free(T);
+	
+
+	return 0;
+
+}
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 
 prog: main.o coul-fct1_2.o victoire.o grille.o fonctions_utiles.o
-	gcc -Wall -Wextra main2.o coul-fct1_2.o victoire.o grille.o fonctions_utiles.o -o prog -lm
+	gcc -Wall -Wextra main.o coul-fct1_2.o victoire.o grille.o fonctions_utiles.o -o prog -lm
 main.o: main.c grille.h coul-fct1_2.h victoire.h fonctions_utiles.h
 	gcc -Wall -Wextra -c main.c -lm
 coul-fct1_2.o: coul-fct1_2.c coul-fct1_2.h grille.h
