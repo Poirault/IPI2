5a5fefb840683548e812fa6f751aa3880d67a4d0
Kevin <kevin170895fr@live.fr>
2017-03-10 12:24:58 +0100

    maj doxygen

--- fonctions_utiles.h
+++ fonctions_utiles.h
@@ -9,4 +9,16 @@
 #include <time.h>
 #include <string.h>
 
+/**
+ /file victoire.h
+ /brief On y mettra toutes les fonctions simples, utiles, pour faciliter la lecture
+*/
+
+/**
+ /function nmbre_coup
+ /param m(taille du jeu)
+ /return entier
+ /brief On a été voir sur internet le jeu et on en a conclu que la formule moyenne du nombre de coup 
+en fonction de la taille du jeu de 1,7*tailleJeu
+*/
 int nmbre_coup(int m); 
