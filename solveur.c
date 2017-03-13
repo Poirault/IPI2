c26b1873a58adb9602a5a20de9c08f950b0dc3be
Vartan <vartan741@hotmail.fr>
2017-03-13 18:58:48 +0100

    4

--- solveur.c
+++ solveur.c
@@ -11,26 +11,57 @@
 
 #include "solveur.h"
 
-/*
-solveur(matrix T, pile solution) 
+void solveur(matrix M, int m)
 {
-	// déclarations des variables locales i, g2, ...
-	int i;
-	for (i=0; i<6; i=i+1) 
-		{ // pour toutes les couleurs possibles
-		if (choixPertinent(T,i)) 
+	int j,k,m,g, nbCoup, NbCoupMin=1000;
+	
+
+	char c;
+
+	matrix M;
+	
+	matrice T;
+
+	M=grille(m);
+
+	nbCoup = nmbre_coup(m);
+
+	system("clear");
+
+	while() { //Tant qu'il n'y a pas de victoire ou que le nombre de tour est inférieure ou égale aux nombres de coups
+	int tour=0;	
+		
+		T=composante_conn(M,c,m); //Matrice d'entier pour savoir quoi "colorier"
+
+		for (j = 0; j < m; ++j)
+		{
+			for (k = 0; k < m; ++k)
 			{
-			solution = empiler(solution,c[i]);
-			g2 = propageCouleur(T, c[i]);
-			if 
-			{
-				(terminaison(g2)) uneSolutionTrouvee(solution);
-			}
-			else 
-			{
-				solveur(g2, solution);
-			}
-			depiler(&solution);
+				if(T[j][k]==1)
+				{
+					M[j][k]=c; //on a pas besoin de la fct coloreplace
+				}
 			}
 		}
-}*/
+
+		system("clear");
+
+
+		tour++;
+		
+
+		/* Fonction solveur */
+		if (victoire(M,m,tour,nbCoup)==1)
+		{
+			NbCoupMin=tour;
+			
+		}
+		
+	}
+
+	
+	
+
+	printf("Le nombre de coup minium est : %d\n", NbCoupMin);
+
+}
